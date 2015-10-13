c64b711d26a966aabb2311d2ca2ff5681b4cab35
Max Schwenk <maschwenk@gmail.com>
2015-10-13 00:46:54 -0400

    Adding the google maps view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,47 @@
-angular.module('starter.controllers', ['ngResource'])
+angular.module('starter.controllers', ['ngResource','uiGmapgoogle-maps'])
 
-.controller('DashCtrl', function($scope, Stations) {
+.controller('DashCtrl', function($scope,$cordovaGeolocation, Stations) {
   var vm = this;
   vm.stations = Stations.queryAll();
+
+  var options = {timeout: 10000, enableHighAccuracy: true};
+ 
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+ 
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    vm.map =  new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+  }, function(error){
+    console.log("Could not get location");
+  });
+})
+
+.controller('MapCtl', function($scope,$cordovaGeolocation, Stations) {
+  var vm = this;
+  var options = {timeout: 10000, enableHighAccuracy: true};
+ 
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+ 
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    vm.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+  }, function(error){
+    console.log("Could not get location");
+  });
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
