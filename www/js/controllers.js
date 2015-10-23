5127609f98e4a13e009086e1ef39d14fd395cbf6
Travis Allen <travis.william.allen@gmail.com>
2015-10-23 16:25:47 -0500

    add DashSwipeeCtrl controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -56,6 +56,58 @@ angular.module('starter.controllers', ['ngResource','uiGmapgoogle-maps'])
   });
 })
 
+.controller('DashSwipeeCtrl', function($scope,$cordovaGeolocation, $state, Stations) {
+  var vm = this;
+  vm.stations = Stations.queryAll();
+
+  var options = {timeout: 10000, enableHighAccuracy: true};
+
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    vm.map =  new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    google.maps.event.addListenerOnce(vm.map, 'idle', function() {
+
+      vm.stations.forEach(function(station){
+        var location = new google.maps.LatLng(station.latitude, station.longitude);
+
+        var marker = new google.maps.Marker({
+          map: vm.map,
+          animation: google.maps.Animation.DROP,
+          position: location
+        });
+
+
+
+
+          var infoWindow = new google.maps.InfoWindow({
+          content: station.name +"("+station.lines+")"
+          });
+
+           google.maps.event.addListener(marker, 'click', function(){
+            infoWindow.open(vm.map, marker);
+            $state.go('tab.dash.swipee.station', { stationId: station.id});
+            console.log(station)
+          })
+
+      })
+
+    })
+
+  }, function(error){
+    console.log("Could not get location");
+  });
+})
+
 // .controller('MapCtl', function($scope,$cordovaGeolocation, Stations) {
 //   var vm = this;
 
