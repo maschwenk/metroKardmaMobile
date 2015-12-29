0914fc0f213e2026b1c2096198f9bf257444a8c4
Travis Allen <travis.william.allen@gmail.com>
2015-12-29 07:20:54 -0500

    fix errors in filter.js file

--- www/js/filters.js
+++ www/js/filters.js
@@ -1,4 +1,5 @@
-(function(){
+
+ (function(){
  angular.module('starter.filters', [])
    .filter('titleCase', titleCase);
 
@@ -7,10 +8,10 @@
        var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
 
        input = input.toLowerCase();
-       return input.replace(/[A-Za-z0-9\u00C0-\u00FF][^\s-]*/g, function(match, index, title) {
-         if (index > 0 && index  match.length !== title.length &&
+       return input.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title) {
+         if (index > 0 && index + match.length !== title.length &&
            match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
-           (title.charAt(index  match.length) !== '-' || title.charAt(index - 1) === '-') &&
+           (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
            title.charAt(index - 1).search(/[^\s-]/) < 0) {
            return match.toLowerCase();
          }
@@ -19,8 +20,8 @@
            return match;
          }
 
-         return match.charAt(0).toUpperCase()  match.substr(1);
+         return match.charAt(0).toUpperCase() + match.substr(1);
        });
      }
-   }
+  }
  })();
