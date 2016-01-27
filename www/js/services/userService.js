ab52ae4f5f49e16f7bc6c8737124fdf4a200b805
Travis Allen <travis.william.allen@gmail.com>
2016-01-26 20:27:37 -0500

    add a user service.  Next step is to combine the User service defined in service.js

---
+++ www/js/services/userService.js
@@ -0,0 +1,15 @@
+angular.module('starter.services').factory('userService', function($http) {
+  var o = {
+    users: []
+  };
+
+  o.get = function(id) {
+    return $http.get('http://localhost:3000/users/' + id + '.json').then(function(res){
+        return res.data
+    }, function(err){
+      console.log(err)
+    })
+  }
+
+  return o
+})
